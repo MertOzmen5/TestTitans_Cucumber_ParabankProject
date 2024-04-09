672110b41fdef50de8dfc33ad4212a800524ed53
MertOzmen <avmertozmen93@gmail.com>
2024-04-09 15:01:09 +0300

    Second method has been completed

--- src/test/java/StepDefinitions/US_601_MertStepDefinitions.java
+++ src/test/java/StepDefinitions/US_601_MertStepDefinitions.java
@@ -3,9 +3,14 @@ package StepDefinitions;
 
 import Pages.DialogContent_Mert;
 import Pages.LeftNav_Mert;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebElement;
+
+import javax.xml.crypto.Data;
+import java.util.List;
 
 public class US_601_MertStepDefinitions {
     LeftNav_Mert lfm=new LeftNav_Mert();
@@ -16,9 +21,14 @@ public class US_601_MertStepDefinitions {
     }
 
     @Then("Fill in Registration information as a user and click the register button")
-    public void fillInRegistrationInformationAsAUserAndClickTheRegisterButton() {
-    }
+    public void fillInRegistrationInformationAsAUserAndClickTheRegisterButton(DataTable textAndKeys) {
+        List<List<String>> listText=textAndKeys.asLists(String.class);
 
+        for (int i = 0; i < listText.size() ; i++) {
+            WebElement textElement= dcm.getWebElement(listText.get(i).get(0));
+            dcm.mySendKeys(textElement,listText.get(i).get(1));
+        }
+    }
     @And("User should be register successfullied")
     public void userShouldBeLoginSuccessfullied() {
     }
