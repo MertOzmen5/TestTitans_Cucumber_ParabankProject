2c1ecd20bfbecf54a77fcf986069d2c9f4cc8ca8
Degisiklikler1 <megaozi9517536@gmail.com>
2024-04-11 16:10:28 +0300

    değişiklikler yapıldı

--- src/test/java/StepDefinitions/US_604_OguzhanSteps.java
+++ src/test/java/StepDefinitions/US_604_OguzhanSteps.java
@@ -2,9 +2,11 @@ package StepDefinitions;
 
 import Pages.DialogContent_Oguzhan;
 import Pages.LeftNav_Oguzhan;
-import Pages.ParentPage;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
@@ -17,7 +19,7 @@ public class US_604_OguzhanSteps {
     DialogContent_Oguzhan dc = new DialogContent_Oguzhan();
 
 
-    @And("Click On The Open New Account")
+    @Given("Click On The Open New Account")
     public void clickOnTheOpenNewAccount(DataTable kategoriler) {
         List<String> kategorilerList = kategoriler.asList(String.class);
 
@@ -30,14 +32,13 @@ public class US_604_OguzhanSteps {
 
     }
 
-    @And("Select The Account Type")
+    @When("Select The Account Type")
     public void selectTheAccountType(DataTable accountButonlari) {
         List<String> accountButonlariList = accountButonlari.asList(String.class);
         for (int i = 0; i < accountButonlariList.size(); i++) {
             WebElement webElement = dc.getWebElement(accountButonlariList.get(i));
             dc.verifyContainsText(dc.minimumDepositedAssert, "$100,00");
-//            dc.myClick(dc.checking);
-//            dc.myClick(dc.accountIdSelect);
+//            Select select=new Select(dc.select);
             dc.myClick(dc.openNewAccountButton);
 
 
@@ -45,7 +46,7 @@ public class US_604_OguzhanSteps {
 
     }
 
-    @And("Success Message Should Be Displayed")
+    @Then("Success Message Should Be Displayed")
     public void successMessageShouldBeDisplayed() {
         dc.verifyContainsText(dc.accountSuccessfully, "Congratulations, your account is now open.");
     }
@@ -68,7 +69,8 @@ public class US_604_OguzhanSteps {
         List<String> accountButonlariList2 = accountButonlari2.asList(String.class);
         for (int i = 0; i < accountButonlariList2.size(); i++) {
             WebElement webElement = dc.getWebElement(accountButonlariList2.get(i));
-            dc.myClick(dc.savings);
+            Select select=new Select(dc.select);
+            select.selectByValue("1");
             dc.myClick(dc.accountIdSelect);
             dc.myClick(dc.openNewAccountButton);
 
