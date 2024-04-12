df1381c0e2ed62ca9ac94a7e9e396a587b26bc64
asli003 <z.aslicelik@gmail.com>
2024-04-12 15:59:20 +0300

    LeftNav

---
+++ src/test/java/FeatureFiles/US_608_Asli.feature
@@ -0,0 +1,8 @@
+Feature: Loan Application
+
+  Background:
+    Given Navigate to Parabank website
+    When Enter username and password and click on login button
+    Then User should login successfully
+
+
--- src/test/java/Pages/DialogContent_Asli.java
+++ src/test/java/Pages/DialogContent_Asli.java
@@ -1,4 +1,37 @@
 package Pages;
 
-public class DialogContent_Asli {
+import Utilities.GWD;
+import org.openqa.selenium.support.PageFactory;
+
+public class DialogContent_Asli extends ParentPage{
+
+    public DialogContent_Asli() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
