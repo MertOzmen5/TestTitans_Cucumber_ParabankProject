e634566efe9e2c4ad8bdfb379eaf19b9c30294c2
asli003 <z.aslicelik@gmail.com>
2024-04-17 14:09:16 +0300

    Asli dosyaları eklendi

--- src/test/java/Pages/DialogContent_Asli.java
+++ src/test/java/Pages/DialogContent_Asli.java
@@ -1,6 +1,8 @@
 package Pages;
 
 import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DialogContent_Asli extends ParentPage{
@@ -10,15 +12,50 @@ public class DialogContent_Asli extends ParentPage{
     }
 
 
+        @FindBy(css = "[class='title']")
+     public WebElement accountOverwiew;
+
+        @FindBy(css = "[ng-model='loanRequest.amount']")
+    public WebElement loanAmount;
+
+        @FindBy(css = "[ng-model='loanRequest.downPayment']")
+    public WebElement downPayment;
+
+        @FindBy(css = "[ng-model='loanRequest.fromAccountId']")
+    public WebElement fromAccount;
+
+        @FindBy(css = "[value='Apply Now']")
+    public WebElement applyNowButton;
+
+    @FindBy(xpath = "//a[text()='Request Loan']")
+    public WebElement noTranscation;
+
+    @FindBy(xpath = "//*[@id='rightPanel']/div/div/div/p[1]")
+    public WebElement loanApproved;
 
 
+    public WebElement getWebElement(String strElement) {
 
+        switch (strElement) {
+            case "accountOverwiew":
+                return this.accountOverwiew;
+            case "loanAmount":
+                return this.loanAmount;
+            case "downPayment":
+                return this.downPayment;
+            case "fromAccount":
+                return this.fromAccount;
+            case "applyNowButton":
+                return this.applyNowButton;
+            case "noTranscation":
+                return this.noTranscation;
+            case "loanApproved":
+                return this.loanApproved;
 
+        }
 
-
-
-
-
+        return null;
+    }
 
 
 
